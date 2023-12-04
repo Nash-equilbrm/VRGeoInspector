809ea63fe3a517c07a93d84b01f08e8fa3227c4d
Hoài Nam Đỗ <dohoainam17052001@gmail.com>
2023-12-04 13:13:28 +0700

    first commit

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1878544.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 156217449, 155833821, 154641939, 155833396, 155830832, 156044577, 155830419, 155833632, 156360847, 156361396, 156362895, 156511676, 157154386 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
