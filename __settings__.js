ad154d089257f19133017f9eb010dfa4ee1672c2
Hoài Nam Đỗ <dohoainam17052001@gmail.com>
2024-05-06 19:23:15 +0700

    *fix lighting + arrow click

--- __settings__.js
+++ __settings__.js
@@ -8,7 +8,7 @@ window.CONTEXT_OPTIONS = {
     'deviceTypes': [`webgl2`, `webgl1`],
     'powerPreference': "default"
 };
-window.SCRIPTS = [ 177128374, 177128372, 177128365, 177128370, 177128369, 177128373, 177128368, 177128371, 177128376, 177128378, 177128380, 177128381, 177128440, 177128442, 177128443, 177128446, 177128447, 177128448, 177128449, 177128460, 177128465, 177128466, 177128467, 177128468, 177128479, 177132337 ];
+window.SCRIPTS = [ 177128374, 177128372, 177128365, 177128370, 177128369, 177128373, 177128368, 177128371, 177128376, 177128378, 177128380, 177128381, 177128440, 177128442, 177128443, 177128446, 177128447, 177128448, 177128449, 177128460, 177128465, 177128466, 177128467, 177128468, 177128479, 177132337, 177143015 ];
 window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
@@ -18,5 +18,5 @@ window.INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 window.PRELOAD_MODULES = [
-    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/177128462/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/177128463/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/177128461/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/177142889/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/177142890/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/177142888/1/ammo.js', 'preload' : true},
 ];
